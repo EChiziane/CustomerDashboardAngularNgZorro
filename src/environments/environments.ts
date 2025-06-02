dcfbad446fe288b2e647adaa06cfd1e3d8e7e33e
Ednilson Chiziane <eddybruno43@gmail.com>
2025-06-02 14:57:45 +0200

    feat()Adding carload endpoint

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:8080',
-  baseURL1: 'https://watersytemmanagementjava-production.up.railway.app'
+  baseURL1: 'http://localhost:8080',
+  baseURL: 'https://watersytemmanagementjava-production.up.railway.app'
 };
