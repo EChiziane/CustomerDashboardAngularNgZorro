125d78e39d23de26a909e91c5aa36c9ca7ea36b6
Chiziane <eddybruno43@gmail.com>
2025-06-12 15:18:50 +0200

    Feat():changing envrioment

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL2: 'http://localhost:8080',
+  baseURL: 'http://localhost:8080',
   baseURL1: 'http://auth-service-java-spring-angular-production.up.railway.app',
-  baseURL:'https://auth-service-java-spring-angular-1.onrender.com'
+  baseURL2:'https://auth-service-java-spring-angular-1.onrender.com'
 };
